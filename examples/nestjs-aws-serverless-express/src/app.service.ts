83475d67f01dac31629a31b361c7ddbba36bffb3
Rennan Stefan Boni <rennanboni@gmail.com>
2020-07-22 20:54:10 -0300

    rename project

--- examples/nestjs-aws-serverless-express/src/app.service.ts
+++ examples/nestjs-aws-serverless-express/src/app.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): { msg: string } {
-    return { msg: 'Hello World!' };
+    return {
+      msg: 'Hello World! from Nest.js with "Express.js" engine'
+    };
   }
 }
